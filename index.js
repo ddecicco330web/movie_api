a126e25fe9bd25bff0e7c65be88bc5454b9ccfc4
ddecicco330web <ddecicco330@gmail.com>
2023-05-31 17:47:45 -0400

    Switch URI

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ require('./passport.js');
 
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://127.0.0.1:27017/myFlix', { useNewUrlParser: true, useUnifiedTopology: true });
-//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://127.0.0.1:27017/myFlix', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 /////////// Add User ////////////////
