038f58f4f0cb0c87cd6e2531bce908a076404225
ddecicco330web <ddecicco330@gmail.com>
2023-06-26 15:28:57 -0400

    Add JWT auth

--- index.js
+++ index.js
@@ -223,16 +223,20 @@ app.delete(
 );
 
 //////// Get All Movies ////////////
-app.get('/movies', (req, res) => {
-  Movies.find()
-    .then((movies) => {
-      return res.status(200).json(movies);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
+app.get(
+  '/movies',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Movies.find()
+      .then((movies) => {
+        return res.status(200).json(movies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  }
+);
 
 ////// Get Movie by Title //////////
 app.get(
