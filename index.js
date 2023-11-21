5a64c99700ac2c89a182ea00e3fe0baaecf3ba9d
ddecicco330web <ddecicco330@gmail.com>
2023-11-21 15:55:23 -0500

    Change documentation file

--- index.js
+++ index.js
@@ -610,7 +610,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/documentation', (req, res) => {
-  res.sendFile(__dirname + '/public/documentation.html');
+  res.sendFile(__dirname + '/out/index.html');
 });
 
 //////// Error Checking ////////////
